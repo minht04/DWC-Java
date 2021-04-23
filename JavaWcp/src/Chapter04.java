93614e51aec834b94a99f0569d8d0686d45ce35d
hitomi <sky.force.1013@gmail.com>
2021-04-23 21:43:49 +0900

    [finish]chapter4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 7;
+		String str = "おちあい　ひとみ";
+		System.out.println(str);
+
+		str = "なんとかなる！";
+		System.out.println(str);
+		System.out.println(LUCKY_NUMBER);
+	}
+
+}
