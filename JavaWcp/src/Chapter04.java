098cbc2a09d5bbb979176346b7131d1674a9997e
hitomi <sky.force.1013@gmail.com>
2021-05-03 14:41:54 +0900

    コメント追加

--- JavaWcp/src/Chapter04.java
+++ JavaWcp/src/Chapter04.java
@@ -2,6 +2,7 @@
 public class Chapter04 {
 	public static void main(String[] args) {
 		final int LUCKY_NUMBER = 7;
+		// 変数の定義と同時に値を代入(=変数の初期化)
 		String str = "おちあい　ひとみ";
 		System.out.println(str);
 		str = "なんとかなる！";
