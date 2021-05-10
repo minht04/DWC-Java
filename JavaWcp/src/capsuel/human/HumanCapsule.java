6f3fa658316e1991ad561e1d5813db69d29e01af
hitomi <sky.force.1013@gmail.com>
2021-05-10 21:20:17 +0900

    [start]chapter12

---
+++ JavaWcp/src/capsuel/human/HumanCapsule.java
@@ -0,0 +1,18 @@
+package capsuel.human;
+
+public class HumanCapsule {
+	private String name;
+	private int age;
+	
+	public HumanCapsule(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+}
